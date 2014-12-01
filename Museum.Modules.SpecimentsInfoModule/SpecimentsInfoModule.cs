5a92c82dfe22509e2dbc14d9a7aed40d9a6653cc
Horizon <professorr@list.ru>
2014-12-01 16:49:01 +0400

    SpecimentsInfoModule init

---
+++ Museum.Modules.SpecimentsInfoModule/SpecimentsInfoModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Museum.Modules.SpecimentsInfoModule
+{
+    class SpecimentsInfoModule
+    {
+
+    }
+
+    /*/// <summary>
+    /// Модуль для работы с абитуриентами и информацией о них
+    /// </summary>
+    [ModuleExport(typeof(AbiturientsInfoModule))]
+    public class AbiturientsInfoModule : IModule
+    {
+        [Import]
+        public IRegionManager RegionManager;
+
+        public void Initialize()
+        {
+            this.RegionManager.RegisterViewWithRegion(RegionNames.MainContentRegion, typeof(TableView));
+            this.RegionManager.RegisterViewWithRegion(RegionNames.MainContentRegion, typeof(AnketaView));
+            //this.RegionManager.RegisterViewWithRegion(RegionNames.MainContentRegion, typeof(IKonkursRowTable));                                     
+        }
+    }*/
+}
